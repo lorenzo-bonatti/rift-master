ee13dca59ffccc736e0146b1b9795e99df918f83
Lorenzo Bonatti <lorenzo.bonatti@3ds.team>
2026-03-24 19:49:18 +0100

    feat: create new function to get set and number from card.public_code

--- src/utils/cards.ts
+++ src/utils/cards.ts
@@ -1,6 +1,23 @@
-// Padding card number as XXX with leading zeros
+import type { Tables } from "../database.types";
+
+/**
+ * Padding card number as XXX with leading zeros
+ * @see getCardSetNumber
+ * @deprecated
+ */
 export function formatCardNumber(cardNumber: number): string {
     return cardNumber.toString().padStart(3, "0");
+}
+
+/**
+ * Extract set code and card number from public code, which is in the format "SFD-227\/221"
+ */
+export function getCardSetNumber(publicCode: string): { set: string; number: string } | null {
+    const [code] = publicCode.split("/");
+    if (!code) return null;
+    const [set, number] = code.split("-");
+    if (!set || !number) return null;
+    return { set, number };
 }
 
 // Split card name into string of words with max 12 characters each, to avoid overflow in the card list
